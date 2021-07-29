c335aacc165e0910dcdc342349c9439e0ae885d6
Jared <jaredblase@gmail.com>
2021-07-30 00:10:46 +0800

    add(logic): drawer opening function

--- app/src/main/java/com/mobdeve/s15/group5/notegeo/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mobdeve.s15.group5.notegeo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdeve.s15.group5.notegeo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.sideMenuBtn.setOnClickListener {
+            view.open()
+        }
     }
 }
