a985ab07e1ed253fcc639b3f3f66b2627f1ec198
Jared <jaredblase@gmail.com>
2021-07-31 01:00:05 +0800

    update(editor view): logic to make the menu appear

--- app/src/main/java/com/mobdeve/s15/group5/notegeo/EditNoteActivity.kt
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/EditNoteActivity.kt
@@ -2,10 +2,18 @@ package com.mobdeve.s15.group5.notegeo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mobdeve.s15.group5.notegeo.databinding.ActivityEditNoteBinding
 
 class EditNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityEditNoteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_note)
+        binding = ActivityEditNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.editorMoreOptions.setOnClickListener {
+            EditorMenuFragment().show(supportFragmentManager, "Editor Menu")
+        }
     }
 }
