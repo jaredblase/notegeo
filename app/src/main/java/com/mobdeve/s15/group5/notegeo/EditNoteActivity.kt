cda25d2af6db2620a9b8a2237de1ee56a63e9c56
Jared <jaredblase@gmail.com>
2021-07-30 10:22:04 +0800

    add(layout): finished editor base layout

---
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/EditNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s15.group5.notegeo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
