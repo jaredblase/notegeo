877ce124484fc41d5673bd59e237e1f6cb62be65
Jared <jaredblase@gmail.com>
2021-08-02 20:48:50 +0800

    add(LabelEntry): class definition

---
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/models/LabelEntry.kt
@@ -0,0 +1,5 @@
+package com.mobdeve.s15.group5.notegeo.models
+
+import androidx.databinding.ObservableBoolean
+
+class LabelEntry(val label: String = "", var isChecked: ObservableBoolean = ObservableBoolean(false))
