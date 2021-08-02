a317fa51bab3267f5fe1203e44cb073cfceabf88
Jared <jaredblase@gmail.com>
2021-08-02 23:41:57 +0800

    add(data): data class and helper

--- app/src/main/java/com/mobdeve/s15/group5/notegeo/DataHelper.kt
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/DataHelper.kt
@@ -1,10 +1,20 @@
 package com.mobdeve.s15.group5.notegeo
 
 import com.mobdeve.s15.group5.notegeo.models.LabelEntry
+import com.mobdeve.s15.group5.notegeo.models.Note
 
 class DataHelper {
     companion object {
         fun loadLabelEntries() =
             arrayListOf(LabelEntry("Sample"), LabelEntry("Hello World"), LabelEntry("Another One"))
+
+        fun loadNotes() = arrayListOf(
+            Note("Sample", "Lorem Ipsum Brodie"),
+            Note(
+                "Hello!",
+                "Just some random text to wee wee. Need to make this note a bit more longer so we can see a difference in the layout"
+            ),
+            Note("Test try...", "Is this cool or what? Kotlin master race OwO")
+        )
     }
 }
