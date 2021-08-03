9357c028203882e3f529ec64c7a50368a1af99f9
Jared <jaredblase@gmail.com>
2021-08-03 09:53:18 +0800

    add(class): thread-safe singleton design

---
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/models/SingletonHolder.kt
@@ -0,0 +1,29 @@
+package com.mobdeve.s15.group5.notegeo.models
+
+/**
+ * A thread-safe singleton implementation.
+ * Code obtained from https://stackoverflow.com/a/45912620
+ */
+open class SingletonHolder<T, A>(creator: (A) -> T) {
+    private var creator: ((A) -> T)? = creator
+    @Volatile private var instance: T? = null
+
+    fun getInstance(arg: A): T {
+        val i = instance
+        if (i != null) {
+            return i
+        }
+
+        return synchronized(this) {
+            val i2 = instance
+            if (i2 != null) {
+                i2
+            } else {
+                val created = creator!!(arg)
+                instance = created
+                creator = null
+                created
+            }
+        }
+    }
+}
