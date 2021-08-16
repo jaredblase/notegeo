9959c130f2418c47d82ac72a2e248289cb138767
Jared <jaredblase@gmail.com>
2021-08-17 03:17:51 +0800

    add(extension): custom text watcher

--- app/src/main/java/com/mobdeve/s15/group5/notegeo/Extensions.kt
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/Extensions.kt
@@ -1,6 +1,16 @@
 package com.mobdeve.s15.group5.notegeo
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 
 fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+class MyWatcher(private val fn: (Editable?) -> Unit) : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+
+    override fun afterTextChanged(s: Editable?) = fn(s)
+}
