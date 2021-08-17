9a655c16bbe93b8a2d8566c85cbaed5b6f61c586
Jared <jaredblase@gmail.com>
2021-08-17 11:03:10 +0800

    add(extension): open keyboard convenience method

--- app/src/main/java/com/mobdeve/s15/group5/notegeo/Extensions.kt
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/Extensions.kt
@@ -1,11 +1,22 @@
 package com.mobdeve.s15.group5.notegeo
 
 import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+fun AppCompatActivity.focusAndOpenKeyboard(v: View) {
+    v.requestFocus()
+    with(getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager) {
+        toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+    }
+}
 
 class MyWatcher(private val fn: (Editable?) -> Unit) : TextWatcher {
     override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
