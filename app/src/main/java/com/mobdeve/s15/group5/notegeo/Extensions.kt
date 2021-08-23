55526aabad245cf6af41c02cfa9f40bbbc2e6ab0
Jared <jaredblase@gmail.com>
2021-08-23 10:02:55 +0800

    refactor(cleanup): removed unused resources.

--- app/src/main/java/com/mobdeve/s15/group5/notegeo/Extensions.kt
+++ app/src/main/java/com/mobdeve/s15/group5/notegeo/Extensions.kt
@@ -5,7 +5,6 @@ import android.content.Context.INPUT_METHOD_SERVICE
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
-import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
